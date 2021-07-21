b4ed24cd663fa1696c92f8b97c8e2781a6941cad
CarsonPC <carsoncoyle2015@gmail.com>
2021-07-21 18:29:15 -0400

    condensing branches

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,20 +13,21 @@ const ticketMasterUrl = "https://app.ticketmaster.com/discovery/v2/events";
 const ticketMasterKey = "tNq308PJQ4YjlzGPiveVZXsWOYlMDGth";
 
 var yesHandler = function (event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  zipCode = postalCode.val().trim();
+    zipCode = postalCode.val().trim();
 
-  if (zipCode.length === 5) {
-    getEventRepos(zipCode);
-    getDrink()
-  } else { 
-    
+    if (zipCode.length === 5) {
+        //getEventRepos(zipCode);
+        //getDrink()
+        //yesButton.on('submit', yesHandler);
+        location.replace('results.html')
+    } else { 
+    console.log('hello')
     return;
-  };
+    };
 
 };
-
 
 
 
@@ -34,19 +35,20 @@ var getEventRepos = function(postalCode) {
     var apiUrl = ticketMasterUrl + '.json?postalCode=' + postalCode + '&apikey=' + ticketMasterKey;
 
     fetch(apiUrl)
-      .then(function (response) {
+    .then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 // displayRepos(data);
                 console.log(data);
+                // window.location.replace('./results.html') works but no data pulled
             });
         } else {
             alert('Error ' + response.statusText);
         }
-      })
-      .catch(function (error) {
+    })
+    .catch(function (error) {
         alert('Unable to connect to TicketMaster');
-      });
+    });
 };
 
 // getEventRepos();
@@ -55,16 +57,17 @@ var getEventRepos = function(postalCode) {
 
 function getDrink() {
 for (let i=0; i<10; i++) {
-  let apiUrlD = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
- fetch(apiUrlD)
-  .then(function (response) {
+    let apiUrlD = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+    fetch(apiUrlD)
+    .then(function (response) {
     if (response.ok) {
         response.json().then(function (data) {
         storedDrinks.push(data)
-      })
+    })
     }
-  }
+}
 )}
 }
 console.log(storedDrinks)
 yesButton.on('click', yesHandler);
+noButton.on('click', yesHandler);
