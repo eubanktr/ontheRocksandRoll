ab58cbcdd35f6f1d3a75ebc8c388190dc6291edc
Gillian Baker <gillianbaker97@gmail.com>
2021-07-21 11:27:32 -0400

    new

--- assets/js/page1script.js
+++ assets/js/page1script.js
@@ -2,5 +2,5 @@ var enter = document.querySelector('postalCode');
 
 enter.addEventListener('keydown', function(event){
     event.preventDefault();
-    window.location.replace("index.html")
+    window.location.replace("index2.html")
 })
