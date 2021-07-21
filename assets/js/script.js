3baa5a1306eb26f91e6b14c10311c22080c70212
Thomas Eubank <eubanktr@gmail.com>
2021-07-21 14:39:02 -0400

    updated modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
 var postalCode = $('#postalCode');
 var yesButton = $('#submitYes');
-var noButton = $('#sumbitNo');
-
-
-
-
-
-
-
+var noButton = $('#submitNo');
+var errModal = document.querySelector('#modal1');
 const ticketMasterUrl = "https://app.ticketmaster.com/discovery/v2/events";
 const ticketMasterKey = "tNq308PJQ4YjlzGPiveVZXsWOYlMDGth";
+var eventNum = 8;
+
+document.addEventListener('DOMContentLoaded', function() {
+M.Modal.init(errModal);
+});
 
 var yesHandler = function (event) {
   event.preventDefault();
@@ -18,18 +17,41 @@ var yesHandler = function (event) {
 
   if (zipCode.length === 5) {
     getEventRepos(zipCode);
-  } else { 
+  } else if (zipCode.length !== 5) { 
     
+    // MODAL POP UP "Must enter a valid 5 digit zip code"
+      var instance = M.Modal.getInstance(errModal);
+      instance.open();
     return;
   };
+};
 
+var noHandler = function (event) {
+  event.preventDefault();
+
+  zipCode = postalCode.val().trim();
+
+  if (zipCode.length === 5) {
+    getEventRepos(zipCode);
+  } else if(zipCode.length !==5) { 
+    
+    var instance = M.Modal.getInstance(errModal);
+      instance.open();
+    
+      return;
+  
+    };
 };
 
 
 
 
+
+
+
+
 var getEventRepos = function(postalCode) {
-    var apiUrl = ticketMasterUrl + '.json?postalCode=' + postalCode + '&apikey=' + ticketMasterKey;
+    var apiUrl = ticketMasterUrl + '.json?postalCode=' + postalCode + '&size=' + eventNum + '&apikey=' + ticketMasterKey;
 
     fetch(apiUrl)
       .then(function (response) {
@@ -47,8 +69,30 @@ var getEventRepos = function(postalCode) {
       });
 };
 
+var displayRepos = function (events, zipCode) {
+  if (events.length === 0) {
+    // Create dynamic container to display "No events found in + zipCode"
+    return;
+  }
+
+  // Dynamic container created 4 lines above displays events in the area.
+
+  for (var i = 0; i < events.length; i++) {
+
+  }
+
+
+
+
+
+
+
+
+}
+
 // getEventRepos();
 
 // console.log(getEventRepos);
 
 yesButton.on('click', yesHandler);
+noButton.on('click', noHandler);
