d3f15b9a0b6bc7fda91faf5a886ef6614a213162
Gillian Baker <gillianbaker97@gmail.com>
2021-07-22 13:09:15 -0400

    new

--- assets/js/page1script.js
+++ assets/js/page1script.js
@@ -3,14 +3,7 @@ const ticketMasterKey = "tNq308PJQ4YjlzGPiveVZXsWOYlMDGth";
 var eventNum = 8;
 let listStart = document.getElementById('list-start')
 var storedDrinks = [];
-var backButton = $('#backButton')
-
-var goBack = function Back (event) {
-    event.preventDefault();
-    window.location.replace("index.html");
-  };
-  
-  backButton.addEventListener('click', goBack);
+var backButton = $('#backButton');
 
 
 const zipInput = localStorage.getItem('userZip')
@@ -35,44 +28,9 @@ var getEventRepos = function(postalCode) {
     });
 };
 
-var displayEvents = function (events) {
-if (events.length === 0) {
-    // Create dynamic container to display "No events found in + zipCode"
-    // var instance = M.Modal.getInstance(bummerModal);
-    //   instance.open();
-    return;
-}
-
-  // Dynamic container created 4 lines above displays events in the area.
 
 
-    //var eventList = document.getElementById('eventList');
-    //var eventEl = document.createElement('ul');
-    for (var i = 0; i < events._embedded.events.length; i++) {
-    var eventL = document.createElement('li')
-    eventL.textContent = events._embedded.events[i].name
-    listStart.append(eventL)
-    console.log(eventL)
-    // eventEl.classList = 'STYLING HERE';
-    // eventList.appendChild(eventEl);
-    //eventEl.append(eventL)
-    //eventList.append(eventEl)
-    }
-}
-function getDrink() {
-    for (let i=0; i<10; i++) {
-        let apiUrlD = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
-        fetch(apiUrlD)
-        .then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-            storedDrinks.push(data)
-            // execute a function you'll need to define, when ran you will pass stored drinks as an argument.
-        })
-        };
-    }
-    )}
-    }
+
 console.log(storedDrinks)
-getDrink()
+//getDrink()
 getEventRepos(zipInput)
--- assets/js/page2script.js
+++ assets/js/page2script.js
@@ -1,31 +1,158 @@
- /* function getData(url){
-    fetch(url)  // read over http and request this data from a server //
-    .then(response) => {
-        console.log(response);  // console log only the data we need //
-        if (!response.ok){
-            throw response;  // enables us to use the catch block //
+var postalCode = $('#postalCode');
+var yesButton = $('#submitYes');
+var noButton = $('#submitNo');
+var errModal = document.querySelector('#modal1');
+var bummerModal = document.querySelector('#modal2');
+const ticketMasterUrl = "https://app.ticketmaster.com/discovery/v2/events";
+const ticketMasterKey = "tNq308PJQ4YjlzGPiveVZXsWOYlMDGth";
+var eventNum = 8;
+var storedDrinks = [];
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+M.Modal.init(errModal);
+});
+document.addEventListener('DOMContentLoaded', function() {
+    M.Modal.init(bummerModal);
+});
+
+var yesHandler = function (event) {
+    event.preventDefault();
+
+    zipCode = postalCode.val().trim();
+  if (zipCode.length === 5) {
+    
+    //getDrink();
+    localStorage.setItem('userZip', zipCode)
+    location.replace('./results.html')
+    //getEventRepos(zipCode);
+    //displayEvents(zip)
+  } else if (zipCode.length !== 5) { 
+    // MODAL POP UP "Must enter a valid 5 digit zip code"
+      var instance = M.Modal.getInstance(errModal);
+      instance.open();
+    return;
+  };
+};
+
+var noHandler = function (event) {
+  event.preventDefault();
+
+  zipCode = postalCode.val().trim();
+
+  if (zipCode.length === 5) {
+    // location.replace('./results.html')
+    // displayEvents(zip)
+    // getEventRepos(zipCode);
+    localStorage.setItem('userZip', zipCode)
+    location.replace('./nopage.html')
+  } else if(zipCode.length !==5) { 
+    
+    var instance = M.Modal.getInstance(errModal);
+      instance.open();
+    
+      return;
+  
+    };
+};
+
+
+
+
+
+
+
+
+var getEventRepos = function(postalCode) {
+    var apiUrl = ticketMasterUrl + '.json?postalCode=' + postalCode + '&size=' + eventNum + '&apikey=' + ticketMasterKey;
+
+    fetch(apiUrl)
+    .then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayEvents(data, postalCode);
+                console.log(data);
+                // window.location.replace('./results.html') works but no data pulled
+            });
+        } else {
+            alert('Error ' + response.statusText);
         }
-        return response.json();
-
     })
-
-    .then(function(data) {
-        console.log(data);  // access to the important information to render display //
-    })
-
-    .catch(function(error) {   
-        console.error(error)
+    .catch(function (error) {
+        alert('Unable to connect to TicketMaster');
     });
-} 
+};
+
+var displayEvents = function (events) {
+if (events.length === 0) {
+    // Create dynamic container to display "No events found in + zipCode"
+    // var instance = M.Modal.getInstance(bummerModal);
+    //   instance.open();
+    return;
+}
+
+  // Dynamic container created 4 lines above displays events in the area.
+
+
+    var eventList = document.getElementById('eventList');
+    var eventEl = document.createElement('ul');
+    for (var i = 0; i < events.length; i++) {
+    var eventL = document.createElement('li')
+    eventL.textContent = events._embedded.events[i].name
+    console.log(eventL)
+    // eventEl.classList = 'STYLING HERE';
+    // eventList.appendChild(eventEl);
+    eventEl.append(eventL)
+    eventList.append(eventEl)
+}
+}
+
+// getEventRepos();
+
+// console.log(getEventRepos);
+
+for (i = 0; i < 5; i++){
+function getDrink() {
+for (let i=0; i<10; i++) {
+    let apiUrlD = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+    fetch(apiUrlD)
+    .then(function (response) {
+    if (response.ok) {
+        response.json().then(function (data) {
+        storedDrinks.push(data)
+        // execute a function you'll need to define, when ran you will pass stored drinks as an argument.
+    })
+    };
+}
+)}
+}
+
+var displayDrinks = function drinkFxn (events) {
+  if (events.length === 0) {
+      // Create dynamic container to display "No drinks found in + zipCode"
+      return;
+  }
+  
+    // Dynamic container created 4 lines above displays drinks.
+  
+  
+      var drinkList = document.querySelector('.Drink-List');
+      var drinkEl = document.createElement('ul');
+      for (var i = 0; i < events.length; i++) {
+      var drinkL = document.createElement('li')
+      drinkL.textContent = events._embedded.events[i].name
+      console.log(drinkL)
+      // drinkEl.classList = 'STYLING HERE';
+      // drinkList.appendChild(drinkEl);
+      drinkEl.append(drinkL)
+      drinkList.append(drinkEl)
+  }
+  }
 
 
 
-URL's to generate:
-    https://app.ticketmaster.com/{package}/{version}/{resource}.json?apikey=**{API key}
-
-    example: 
-    https://app.ticketmaster.com/discovery/v1/events.json?apikey=4dsfsf94tyghf85jdhshwge334
-
-    cocktail recipe:
-    www.thecocktaildb.com/api/json/v1/1/random.php
- */
+console.log(storedDrinks);
+yesButton.on('click', yesHandler);
+noButton.on('click', noHandler);
+};
