4d6df6e568d9eecf9a205f2e7cf5527667da3731
Gillian Baker <gillianbaker97@gmail.com>
2021-07-22 23:32:21 -0400

    new

--- assets/js/page1script.js
+++ assets/js/page1script.js
@@ -1,21 +1,21 @@
-const ticketMasterUrl = "https://app.ticketmaster.com/discovery/v2/events";
-const ticketMasterKey = "tNq308PJQ4YjlzGPiveVZXsWOYlMDGth";
-var eventNum = 8;
-let listStart = document.getElementById('list-start')
+const ticketMasterUrl = "https://app.ticketmaster.com/discovery/v2/events"; //to call the ticketmaster api
+const ticketMasterKey = "tNq308PJQ4YjlzGPiveVZXsWOYlMDGth"; // to call the cocktail api
+var eventNum = 8; 
+let listStart = document.getElementById('list-start') //linking to the index
 var storedDrinks = [];
-var backButton = $('#backButton');
+var backButton = $('#backButton'); //linking a button the the index
 
 
-const zipInput = localStorage.getItem('userZip')
-console.log(zipInput)
-var getEventRepos = function(postalCode) {
-    var apiUrl = ticketMasterUrl + '.json?postalCode=' + postalCode + '&size=' + eventNum + '&apikey=' + ticketMasterKey;
+const zipInput = localStorage.getItem('userZip') // getting the Zipcode
+console.log(zipInput) 
+var getEventRepos = function(postalCode) {     //making the postal code function
+    var apiUrl = ticketMasterUrl + '.json?postalCode=' + postalCode + '&size=' + eventNum + '&apikey=' + ticketMasterKey;  //concatenating parts of the API url
 
-    fetch(apiUrl)
+    fetch(apiUrl) 
     .then(function (response) {
-        if (response.ok) {
+        if (response.ok) { //making sure we get a valid response
             response.json().then(function (data) {
-                displayEvents(data, postalCode);
+                displayEvents(data, postalCode);  //displaying the postal code 
                 console.log(data);
                 // window.location.replace('./results.html') works but no data pulled
             });
@@ -28,73 +28,12 @@ var getEventRepos = function(postalCode) {
     });
 };
 
-var displayEvents = function (events) {
-    if (events.length === 0) {
-        return
-    }
-    //var eventList = document.getElementById('eventList');
-    //var eventEl = document.createElement('ul');
-    for (var i = 0; i < events._embedded.events.length; i++) {
-    var eventL = document.createElement('li');
-    var eventUrl = document.createElement ('a');
-    var pageBrk = document.createElement('br');
-    var infoIcon = document.createElement('i');
-    var icon = 'info'
-    var eventText = '  Click here for more info!' // document.createTextNode('Click Here to Buy Tickets!');
-    eventL.textContent = events._embedded.events[i].name
-    eventUrl.textContent = events._embedded.events[i].url
-    listStart.append(eventL);
-    eventL.append(pageBrk);
-    eventL.append(infoIcon);
-    eventL.append(eventUrl);
-    // console.log(eventL)
-    eventL.classList = 'orange-text collection-item';
-    infoIcon.classList = 'tiny material-icons';
-    eventUrl.setAttribute('target', '_blank');
-    eventUrl.setAttribute(`href`, eventUrl.textContent);
-    eventUrl.innerHTML = eventText;
-    infoIcon.innerHTML = icon;
-    }
-}
 
-for (i = 0; i < 5; i++){
-function getDrink() {
-    for (let i=0; i<10; i++) {
-        let apiUrlD = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
-        fetch(apiUrlD)
-        .then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-            storedDrinks.push(data)
-            // execute a function you'll need to define, when ran you will pass stored drinks as an argument.
-        })
-        };
-    }
-    )}
-    }
-}
 
-    var displayDrinks = function drinkFxn (events) {
-        if (events.length === 0) {
-            return
-        }
-        //var eventList = document.getElementById('eventList');
-        //var eventEl = document.createElement('ul');
-        for (var i = 0; i < events._embedded.events.length; i++) {
-        var drinkL = document.createElement('li');
-        var drinkUrl = document.createElement ('a');
-        var pageBrk = document.createElement('br');
-        drinkL.textContent = events._embedded.events[i].name
-        drinkUrl.textContent = events._embedded.events[i].url
-        listStart.append(drinkL);
-        drinkL.append(pageBrk);
-        drinkL.append(drinkUrl);
-        drinkL.classList = 'orange-text collection-item';
-        drinkUrl.setAttribute('target', '_blank');
-        drinkUrl.setAttribute(`href`, drinktUrl.textContent);
-        drinkUrl.innerHTML = drinkText;
-        }
-    }
-console.log(storedDrinks)
-//getDrink()
+
+
+
+
+
+
 getEventRepos(zipInput)
--- assets/js/page2script.js
+++ assets/js/page2script.js
@@ -9,23 +9,20 @@ var storedDrinks = [];
 
 
 document.addEventListener('DOMContentLoaded', function() {
-M.Modal.init(errModal);
+M.Modal.init(errModal);  //alerts when we have not imputted a 5-digit zip code
 });
 document.addEventListener('DOMContentLoaded', function() {
-    M.Modal.init(bummerModal);
+    M.Modal.init(bummerModal);  // alerts when we have nothing happpening in that zip code
 });
 
 var yesHandler = function (event) {
     event.preventDefault();
 
-    zipCode = postalCode.val().trim();
+    zipCode = postalCode.val().trim(); //trims white space off the entry for the zipcode
   if (zipCode.length === 5) {
     
-    //getDrink();
-    localStorage.setItem('userZip', zipCode)
-    location.replace('./results.html')
-    //getEventRepos(zipCode);
-    //displayEvents(zip)
+    localStorage.setItem('userZip', zipCode) //stores the ZipCode
+    location.replace('./results.html') // redirects to the results page
   } else if (zipCode.length !== 5) { 
     // MODAL POP UP "Must enter a valid 5 digit zip code"
       var instance = M.Modal.getInstance(errModal);
@@ -39,10 +36,7 @@ var noHandler = function (event) {
 
   zipCode = postalCode.val().trim();
 
-  if (zipCode.length === 5) {
-    // location.replace('./results.html')
-    // displayEvents(zip)
-    // getEventRepos(zipCode);
+  if (zipCode.length === 5) {   // if the length of the zip code is 5 then store it and replace the page with the next page
     localStorage.setItem('userZip', zipCode)
     location.replace('./nopage.html')
   } else if(zipCode.length !==5) { 
@@ -58,11 +52,7 @@ var noHandler = function (event) {
 
 
 
-
-
-
-
-var getEventRepos = function(postalCode) {
+var getEventRepos = function(postalCode) {  //concatenating the ticketMaster url 
     var apiUrl = ticketMasterUrl + '.json?postalCode=' + postalCode + '&size=' + eventNum + '&apikey=' + ticketMasterKey;
 
     fetch(apiUrl)
@@ -81,6 +71,35 @@ var getEventRepos = function(postalCode) {
         alert('Unable to connect to TicketMaster');
     });
 };
+
+var displayEvents = function (events) {   // how we wanted to display the events
+  if (events.length === 0) {
+      return
+  }
+  //var eventList = document.getElementById('eventList');
+  //var eventEl = document.createElement('ul');
+  for (var i = 0; i < events._embedded.events.length; i++) {
+  var eventL = document.createElement('li');
+  var eventUrl = document.createElement ('a');
+  var pageBrk = document.createElement('br');
+  var infoIcon = document.createElement('i');
+  var icon = 'info'
+  var eventText = '  Click here for more info!' // document.createTextNode('Click Here to Buy Tickets!');
+  eventL.textContent = events._embedded.events[i].name
+  eventUrl.textContent = events._embedded.events[i].url
+  listStart.append(eventL);
+  eventL.append(pageBrk);
+  eventL.append(infoIcon);
+  eventL.append(eventUrl);
+  // console.log(eventL)
+  eventL.classList = 'orange-text collection-item';
+  infoIcon.classList = 'tiny material-icons';
+  eventUrl.setAttribute('target', '_blank');
+  eventUrl.setAttribute(`href`, eventUrl.textContent);
+  eventUrl.innerHTML = eventText;
+  infoIcon.innerHTML = icon;
+  }
+}
 
 
 // getEventRepos();
@@ -114,6 +133,8 @@ var populateCards = function (storedDrinks) {
     var imgContainer = document.createElement('img');
     var cardName = document.createElement('span');
     var cardContent = document.createElement('div');
+    //var ingredList = document.createElement('ul');
+    //var drinking = document.createElement('li');
 
     container.append(containerSizing);
     containerSizing.append(card);
@@ -133,12 +154,6 @@ var populateCards = function (storedDrinks) {
 
   };
 };
-
-
-
-
-
-
 
 var displayDrinks = function drinkFxn (events) {
   if (events.length === 0) {
@@ -161,6 +176,9 @@ var displayDrinks = function drinkFxn (events) {
   drinkUrl.innerHTML = drinkText;
   }
 };
+
+
+
 
 console.log(storedDrinks);
 yesButton.on('click', yesHandler);
